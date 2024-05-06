7761cb4bfb1f4d554dea7116ad147a5a59e0241a
Adet <adet@Adetias-MacBook-Pro.local>
2024-05-06 21:37:07 +0700

    set thread name

--- src/main/java/com/github/adet/section2/Main.java
+++ src/main/java/com/github/adet/section2/Main.java
@@ -6,6 +6,8 @@ public class Main {
 
         Thread thread = new Thread(() -> System.out.println("we are in a new thread " + Thread.currentThread().getName()));
 
+        thread.setName("New Worker Thread");
+
         System.out.println("we are in thread " + Thread.currentThread().getName() + " before starting a new thread.");
 
         thread.start();
