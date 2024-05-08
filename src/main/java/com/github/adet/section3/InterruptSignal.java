cf484336da832fae57152c3b92a90944d775c889
Adet <adet@Adetias-MacBook-Pro.local>
2024-05-08 17:43:06 +0700

    set a thread as daemon

--- src/main/java/com/github/adet/section3/InterruptSignal.java
+++ src/main/java/com/github/adet/section3/InterruptSignal.java
@@ -6,6 +6,7 @@ public class InterruptSignal {
 
     public static void main(String[] args) {
         Thread thread = new Thread(new LongComputationTask(new BigInteger("20000"), new BigInteger("10000000")));
+        thread.setDaemon(true);
         thread.start();
         thread.interrupt();
     }
@@ -28,10 +29,6 @@ public class InterruptSignal {
         private BigInteger pow(BigInteger base, BigInteger power) {
             BigInteger result = BigInteger.ONE;
             for (BigInteger i = BigInteger.ZERO; i.compareTo(power) != 0; i = i.add(BigInteger.ONE)) {
-                if (Thread.currentThread().isInterrupted()) {
-                    System.out.println("Prematurely interrupted computation");
-                    return BigInteger.ZERO;
-                }
                 result = result.multiply(base);
             }
             return result;
