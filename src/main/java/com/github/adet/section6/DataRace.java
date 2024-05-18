2f8e4532f8d2e5dd3cac76a7a56b376c772a7a5f
Adet <adet@Adetias-MacBook-Pro.local>
2024-05-18 14:19:39 +0700

    volatile as data race solution

---
+++ src/main/java/com/github/adet/section6/DataRace.java
@@ -0,0 +1,41 @@
+package com.github.adet.section6;
+
+public class DataRace {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        SharedClass sharedClass = new SharedClass();
+
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < Integer.MAX_VALUE; i++) {
+                sharedClass.increment();
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            for (int i = 0; i < Integer.MAX_VALUE; i++) {
+                sharedClass.checkForDataRace();
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+    }
+
+    public static class SharedClass {
+
+        private volatile int x;
+        private volatile int y;
+
+        public void increment() {
+            x++;
+            y++;
+        }
+
+        public void checkForDataRace() {
+            if (y > x) {
+                System.out.println("y > x - Data Race is detected");
+            }
+        }
+    }
+}
