362ac97fe9801545701a2026e113f43dd589e715
Adet <adet@Adetias-MacBook-Pro.local>
2024-05-18 14:55:42 +0700

    deadlock solution

--- src/main/java/com/github/adet/section6/Deadlock.java
+++ src/main/java/com/github/adet/section6/Deadlock.java
@@ -81,9 +81,9 @@ public class Deadlock {
         }
 
         public void takeRoadB() {
-            synchronized (roadB) {
-                System.out.println("Road B is locked by " + Thread.currentThread().getName());
-                synchronized (roadA) {
+            synchronized (roadA) {
+                System.out.println("Road A is locked by " + Thread.currentThread().getName());
+                synchronized (roadB) {
                     System.out.println("Train is passing through road B");
                     try {
                         Thread.sleep(1);
