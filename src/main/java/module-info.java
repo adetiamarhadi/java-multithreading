e54e308c604a75e16f861da5df394bd75109a151
Adet <adet@Adetias-MacBook-Pro.local>
2024-05-21 09:09:26 +0700

    ui app example tryLock

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module Test {
+
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires jdk.httpserver;
+    requires java.desktop;
+
+    opens com.github.adet.section7;
+}
