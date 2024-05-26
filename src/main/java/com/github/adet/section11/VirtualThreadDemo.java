63e0ea6160c573527699a2cfd8278d0d2d5e0d68
Adet <adet@Adetias-MacBook-Pro.local>
2024-05-26 21:12:08 +0700

    to see max worker

--- src/main/java/com/github/adet/section11/VirtualThreadDemo.java
+++ src/main/java/com/github/adet/section11/VirtualThreadDemo.java
@@ -1,14 +1,28 @@
 package com.github.adet.section11;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class VirtualThreadDemo {
+
+    private static final int NUMBER_OF_VIRTUAL_THREAD = 1000;
 
     public static void main(String[] args) throws InterruptedException {
 
         Runnable runnable = () -> System.out.println("Inside thread: " + Thread.currentThread());
 
-        Thread platformThread = Thread.ofVirtual().unstarted(runnable);
+        List<Thread> virtualThreads = new ArrayList<>();
+        for (int i = 0; i < NUMBER_OF_VIRTUAL_THREAD; i++) {
+            Thread virtualThread = Thread.ofVirtual().unstarted(runnable);
+            virtualThreads.add(virtualThread);
+        }
 
-        platformThread.start();
-        platformThread.join();
+        for (Thread thread : virtualThreads) {
+            thread.start();
+        }
+
+        for (Thread thread : virtualThreads) {
+            thread.join();
+        }
     }
 }
