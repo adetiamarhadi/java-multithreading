8d555e301cbaea9e1df1ce276e436eddb5ed7522
Adet <adet@Adetias-MacBook-Pro.local>
2024-05-26 21:16:29 +0700

    with blocking task

--- src/main/java/com/github/adet/section11/VirtualThreadDemo.java
+++ src/main/java/com/github/adet/section11/VirtualThreadDemo.java
@@ -5,15 +5,13 @@ import java.util.List;
 
 public class VirtualThreadDemo {
 
-    private static final int NUMBER_OF_VIRTUAL_THREAD = 1000;
+    private static final int NUMBER_OF_VIRTUAL_THREAD = 100;
 
     public static void main(String[] args) throws InterruptedException {
 
-        Runnable runnable = () -> System.out.println("Inside thread: " + Thread.currentThread());
-
         List<Thread> virtualThreads = new ArrayList<>();
         for (int i = 0; i < NUMBER_OF_VIRTUAL_THREAD; i++) {
-            Thread virtualThread = Thread.ofVirtual().unstarted(runnable);
+            Thread virtualThread = Thread.ofVirtual().unstarted(new BlockingTask());
             virtualThreads.add(virtualThread);
         }
 
@@ -23,6 +21,20 @@ public class VirtualThreadDemo {
 
         for (Thread thread : virtualThreads) {
             thread.join();
+        }
+    }
+
+    private static class BlockingTask implements Runnable {
+
+        @Override
+        public void run() {
+            System.out.println("Inside thread: " + Thread.currentThread() + " before blocking call");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Inside thread: " + Thread.currentThread() + " after blocking call");
         }
     }
 }
