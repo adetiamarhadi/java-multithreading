e8568dd9d82d5cbb1b8282604c88b8fbcd342842
Adet <adet@Adetias-MacBook-Pro.local>
2024-05-28 09:18:27 +0700

    IO operation using virtual thread with intensive operation

--- src/main/java/com/github/adet/section11/IOBoundApp.java
+++ src/main/java/com/github/adet/section11/IOBoundApp.java
@@ -18,7 +18,11 @@ public class IOBoundApp {
     private static void performTasks() {
         try (ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor()) {
             for (int i = 0; i < NUMBER_OF_TASKS; i++) {
-                executorService.submit(IOBoundApp::blockingIOOperation);
+                executorService.submit(() -> {
+                    for (int j = 0; j < 100; j++) {
+                        blockingIOOperation();
+                    }
+                });
             }
         }
     }
@@ -26,7 +30,7 @@ public class IOBoundApp {
     private static void blockingIOOperation() {
         System.out.println("Executing a blocking task from thread: " + Thread.currentThread());
         try {
-            Thread.sleep(1_000);
+            Thread.sleep(10);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
